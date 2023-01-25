11e04367f1a7e91361130c23b7daac763c93e724
Simon <simonlucas1996@gmail.com>
2023-01-25 13:54:05 -0300

    Add pinia-orm examples

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
+
 </script>
 
 <template>
@@ -19,6 +20,7 @@ import HelloWorld from "./components/HelloWorld.vue";
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/examples">Examples</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/examples/ExamplesView.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import ActivityType from "../../models/ActivityType";
+  import { useRepo } from 'pinia-orm'
+  const activityTypeRepo = useRepo(ActivityType)
+  // Getting all users with their todos as relation
+  activityTypeRepo.save(
+    [
+      {
+        id: 1,
+        name: "Proyectos",
+        description: "Cualquier tipo de proyecto"
+      }
+    ]
+  );
+  const activityTypes = activityTypeRepo.all()
+</script>
+
+<template>
+  Activity Types
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>Description</th>
+    </tr>
+    <tr v-for="activityType in activityTypes">
+      <td>{{ activityType.name }}</td>
+      <td>{{ activityType.description }}</td>
+    </tr>
+  </table>
+</template>
