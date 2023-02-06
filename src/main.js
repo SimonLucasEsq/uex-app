5f86dff1a2dbb1ec23174788454c1b61bfdc7d18
Simon <simonlucas1996@gmail.com>
2023-02-06 15:13:38 -0300

    Remove pinia orm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { createORM } from 'pinia-orm';
 
 import App from "./App.vue";
 import router from "./router";
@@ -9,9 +8,8 @@ import axios from './plugins/axios'
 import "./assets/main.css";
 
 const app = createApp(App);
-const pinia = createPinia().use(createORM())
 
-app.use(pinia);
+app.use(createPinia());
 app.use(router);
 app.use(axios, {
   baseUrl: 'http://localhost:3000/api/',
