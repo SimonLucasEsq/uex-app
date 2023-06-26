bf8b151eccb17509a46fe571c878f9578a5f1e4a
Magali Cabrera <magymonse24@gmail.com>
2023-06-25 22:19:16 -0400

    Improve file inmport input. Added note info about how to import

--- src/components/ImportProfessor.vue
+++ src/components/ImportProfessor.vue
@@ -16,6 +16,7 @@ const emit = defineEmits([
 ])
 
 const toast = useToast()
+const panel = ref(0)
 
 const updateModelValue = val => {
   emit('update:isDialogVisible', val)
@@ -59,18 +60,56 @@ const importarCSV = async () => {
 
 <template>
   <VDialog
-    max-width="500"
+    max-width="900"
     :model-value="props.isDialogVisible"
     @update:model-value="updateModelValue"
   >
     <DialogCloseBtn @click="updateModelValue(false)" />
     <VCard>
       <VCardText>
-        <input
-          type="file"
-          accept=".csv"
-          @change="seleccionarArchivo"
+        <VExpansionPanels
+          v-model="panel"
+          class="no-icon-rotate"
         >
+          <VExpansionPanel>
+            <VExpansionPanelTitle>
+              <VIcon
+                size="18"
+                icon="tabler-alert-triangle"
+                color="warning"
+              />
+              ¡Importante! Para importar datos correctamente, asegúrate de seguir estas pautas:
+
+            </VExpansionPanelTitle>
+            <VExpansionPanelText>
+              <VAlert
+                border="start"
+                border-color="info"
+              >
+                <ul>
+                  <li>Solo se admiten archivos en formato CSV (valores separados por comas).</li>
+                  <li>El archivo debe contener un encabezado con los siguientes campos: first_name,last_name,sex,email,phone_number,id_card,address y career.</li>
+                  <li>Si se importan algunos registros y otros no, es recomendable crear un nuevo archivo para las filas que no fueron importadas.</li>
+                  <li>Asegúrate de que los valores cumplan con los siguientes criterios:
+                    <ul>
+                      <li>El campo 'sex' debe contener los valores 'masculino' o 'femenino'</li>
+                      <li>El campo 'carrer' debe recibir una lista de carreras separadas por comas y entre comillas dobles. Ej: "Ing. Civil, Ing. Infor.".</li>
+                      <li>El campo 'carrer' debe recibir una lista de carreras con el mismo nombre en que fueron registradas en el sistema.</li>
+                    </ul>
+                  </li>
+                </ul>
+                Asegúrate de que tu archivo cumpla con estos requisitos antes de realizar la importación.
+              </VAlert>
+            </VExpansionPanelText>
+          </VExpansionPanel>
+        </VExpansionPanels>
+      </VCardText>
+      <VCardText>
+        <VFileInput
+          accept="text/csv"
+          label="Seleccionar Archivo"
+          @change="seleccionarArchivo"
+        />
       </VCardText>
       <VCardText class="d-flex justify-end gap-3 flex-wrap">
         <VBtn
@@ -83,3 +122,8 @@ const importarCSV = async () => {
     </VCard>
   </VDialog>
 </template>
+<style type=”text/css”>
+ul {
+  list-style-type: circle;
+}
+</style>
