3a8032470f9d0b9263b63019d4ac505b256c023a
Simon <simonlucas1996@gmail.com>
2023-07-02 20:36:14 -0400

    Add back activity status

---
+++ src/components/ActivityStatusForm.vue
@@ -0,0 +1,150 @@
+<script setup>
+import { useActivityStatus } from '@/composables/activity-status'
+import { useActivityStore } from '@/stores/activity'
+import { requiredValidator } from '@validators'
+import { computed } from 'vue'
+
+const props = defineProps({
+  activity: {
+    type: Object,
+    default: null,
+  },
+  status: {
+    type: String,
+    default: null,
+  },
+  isDialogVisible: {
+    type: Boolean,
+    default: null,
+  },
+})
+
+const emit = defineEmits(['update:isDialogVisible'])
+const refForm = ref()
+const evaluation = ref(props.activity.evaluation)
+const approvedAt = ref(props.activity.approvedAt)
+const resolutionNumber = ref(props.activity.resolutionNumber)
+const store = useActivityStore()
+const { statusActionLabel, prevStatusLabel, getPrevStatus } = useActivityStatus()
+const titleMap = {
+  evaluated: "Evaluación de la Actividad",
+  sent_to_deanery: "Elevar proyecto al Decanato?",
+  approved: "Aprobación de la Actividad",
+  rejected: "Rechazar proyecto?"
+}
+const title = computed(() => {
+  if (getPrevStatus(props.activity.status) === props.status) {
+    return `${prevStatusLabel(props.status)}?`
+  }
+
+  return titleMap[props.status]
+})
+
+async function submit() {
+  const { valid } = await refForm.value.validate()
+  if (!valid) return
+
+  // Custom logic for evaluated status, when evaluation number is less that 60 then status should be declined instead of evaluated
+  let status = props.status === "evaluated" && evaluation.value < 60 ? "declined" : props.status
+  let params = {
+    status,
+    evaluation: evaluation.value,
+    approvedAt: approvedAt.value,
+    resolutionNumber: resolutionNumber.value,
+  }
+  store.api.updateStatus(props.activity.id, params).then(() => {
+    if(store.isValid) {
+      isDialogVisible.value = false
+    }
+  })
+}
+
+const isDialogVisible = computed({
+  get() {
+    return props.isDialogVisible
+  },
+  set(value) {
+    emit('update:isDialogVisible', value)
+  },
+})
+
+</script>
+
+<template>
+  <VDialog
+    v-model="isDialogVisible"
+    persistent
+    class="v-dialog-sm"
+  >
+    <!-- Dialog close btn -->
+    <DialogCloseBtn @click="isDialogVisible = false" />
+
+    <!-- Dialog Content --> 
+    <VCard :title="title">
+      <VCardText class="pb-0">
+        <VForm
+          ref="refForm"
+          @submit.prevent="() => {}"
+        >
+          <!-- Evaluated status fields -->
+          <VCol
+            v-if="status === 'evaluated'"
+            cols="12"
+            class="mb-1"
+          >
+            <VTextField
+              v-model="evaluation"
+              label="Ingrese la valoración obtenida"
+              placeholder="Ingrese la valoración obtenida"
+              type="number"
+              :rules="[requiredValidator]"
+            />
+            <p class="text-caption">Si la evaluación es menor a 60, la actividad será declinada.</p>
+          </VCol>
+          <!-- End Evaluated status fields --> 
+
+          <!-- Approved status fields -->
+          <VCol
+            v-if="status === 'approved'"
+            cols="12"
+          >
+            <AppDateTimePicker
+              v-model="approvedAt"
+              label="Fecha de aprobación"
+              placeholder="Fecha de aprobación"
+              :rules="[requiredValidator]"
+            />
+          </VCol>
+          <VCol
+            v-if="status === 'approved'"
+            cols="12"
+          >
+            <VTextField
+              v-if="status === 'approved'"
+              v-model="resolutionNumber"
+              label="Número de Resolución"
+              placeholder="Número de Resolución"
+              :rules="[requiredValidator]"
+            />
+          </VCol>
+          <!-- End approved status fields --> 
+        </VForm>
+      </VCardText>
+      <VCardText class="d-flex justify-end gap-3 flex-wrap">
+        <VBtn
+          type="submit"
+          @click="submit"
+        >
+          {{ statusActionLabel(status) }}
+        </VBtn>
+        <VBtn
+          color="secondary"
+          variant="tonal"
+          @click="isDialogVisible = false"
+        >
+          Cancelar
+        </VBtn>
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>
---
+++ src/components/ActivityTypeForm.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { useActivityTypeStore } from "@/stores/activity-type";
+import { requiredValidator } from '@validators';
+import { computed, onMounted } from 'vue';
+import router from "../router";
+
+const props = defineProps(['id'])
+const refForm = ref()
+const store = useActivityTypeStore()
+const activityType = computed(() => store.data.record )
+
+async function submit() {
+  const { valid } = await refForm.value.validate()
+  if (valid) {
+    store.api.save().then(() => {
+      if(store.isValid) {
+        router.push({ name: 'activity-types' })
+      }
+    })
+  }
+}
+
+function onCancel() {
+  router.push({ name: 'activity-types' })
+}
+
+onMounted(async () => {
+  if(props.id) {
+    await store.api.find(props.id)
+  } else {
+    store.resetRecord()
+  }
+})
+</script>
+
+<template>
+  <VAlert
+    v-if="store.isInvalid"
+    color="error"
+  >
+    Ops! Error al crear el tipo de Actividad
+    <div v-if="activityType.errors.name">
+      Nombre
+      <VList :items="activityType.errors.name" />
+    </div>
+    <div v-if="activityType.errors.description">
+      Descripción
+      <VList :items="activityType.errors.description" />
+    </div>
+  </VAlert>
+
+  <VCardText class="d-flex align-center flex-wrap gap-4">
+    <VForm
+      ref="refForm"
+      @submit.prevent="() => {}"
+    >
+      <VRow>
+        <VCol cols="12">
+          <VTextField
+            id="name"
+            v-model="activityType.name"
+            label="Nombre"
+            placeholder="Nombre"
+            :rules="[requiredValidator]"
+          />
+        </VCol>
+
+        <VCol cols="12">
+          <VTextField
+            id="description"
+            v-model="activityType.description"
+            label="Descripción"
+            placeholder="Descripción"
+            :rules="[requiredValidator]"
+          />
+        </VCol>
+
+        <VCol
+          cols="12"
+          class="d-flex justify-end gap-4"
+        >
+          <VBtn
+            type="submit"
+            @click="submit"
+          >
+            {{ store.isNew ? "Crear" : "Guardar" }}
+          </VBtn>
+          <VBtn
+            color="secondary"
+            variant="tonal"
+            type="reset"
+            @click.prevent="onCancel"
+          >
+            Cancelar
+          </VBtn>
+        </VCol>
+      </VRow>
+    </VForm>
+  </VCardText>
+</template>
